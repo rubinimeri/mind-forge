db2dce8900382022e00cc1b0b4e0a151c9a360b8
Rubin Imeri <rubinimeri6@gmail.com>
2025-07-23 15:25:57 +0200

    Remove unused arg

--- utils/password.ts
+++ utils/password.ts
@@ -4,7 +4,7 @@ function generateSalt() {
   return crypto.randomBytes(16).toString("hex").normalize();
 }
 
-export function hashAndSaltPassword(password: string, salt: string) {
+export function hashAndSaltPassword(password: string) {
   return new Promise((resolve, reject) => {
     crypto.scrypt(password.normalize(), generateSalt(), 64, (err, hash) => {
       if (err) reject(err)
