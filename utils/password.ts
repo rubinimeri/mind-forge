4fc582b42f85752408ac002516484f6c9e196fd9
Rubin Imeri <rubinimeri6@gmail.com>
2025-07-23 15:24:19 +0200

    Add function for salting and hashing passwords

---
+++ utils/password.ts
@@ -0,0 +1,15 @@
+import crypto from "crypto";
+
+function generateSalt() {
+  return crypto.randomBytes(16).toString("hex").normalize();
+}
+
+export function hashAndSaltPassword(password: string, salt: string) {
+  return new Promise((resolve, reject) => {
+    crypto.scrypt(password.normalize(), generateSalt(), 64, (err, hash) => {
+      if (err) reject(err)
+
+      resolve(hash.toString("hex").normalize())
+    })
+  })
+}
