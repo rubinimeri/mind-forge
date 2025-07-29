708a3d7f605a13c35a0ec0c5a0d66956241d7846
Rubin Imeri <rubinimeri6@gmail.com>
2025-07-29 15:09:51 +0200

    Add proper password validation & save user to session & jwt

--- auth.ts
+++ auth.ts
@@ -3,8 +3,11 @@ import { prisma } from "@/lib/prisma";
 import NextAuth from "next-auth"
 import Credentials from "@auth/core/providers/credentials";
 import {hashAndSaltPassword} from "@/utils/password";
+import { JWT } from "next-auth/jwt";
+import { Session, User } from "next-auth";
 
 export const { auth, handlers, signOut, signIn } =
+  //@ts-ignore
   NextAuth({
     adapter: PrismaAdapter(prisma),
     session: { strategy: "jwt" },
@@ -15,23 +18,71 @@ export const { auth, handlers, signOut, signIn } =
           password: { label: "Password", type: "password" }
         },
         authorize: async (credentials: Partial<Record<"email" | "password", unknown>> | undefined) => {
+          try {
+            if (!credentials || typeof credentials.email !== "string" || typeof credentials.password !== "string") {
+              return null;
+            }
 
-          if (!credentials || typeof credentials.email !== "string" || typeof credentials.password !== "string") {
+            let user = null
+
+            user = await prisma.user.findFirst({where: {email: credentials.email}})
+
+            if (!user)
+              return null
+
+            const hashedPassword = await hashAndSaltPassword(credentials.password, user.salt)
+
+            const passwordsMatch = hashedPassword === user.password;
+
+            if (!passwordsMatch)
+              return null;
+
+            console.log(user)
+
+            return {
+              id: user.id,
+              name: user.name,
+              lastName: user.lastName,
+              email: user.email,
+              image: user.image,
+              createdAt: user.createdAt,
+              updatedAt: user.updatedAt
+            }
+          } catch (err) {
+            console.error(err)
             return null;
           }
-
-          let user = null
-
-          const passwordHash = await hashAndSaltPassword(credentials.password)
-
-          user = await prisma.user.findFirst({ where: { email: credentials.email } })
-
-          if (!user) {
-            throw new Error("Invalid credentials.")
-          }
-
-          return user
         }
       })
-    ]
+    ],
+    pages: {
+      signIn: "/sign-in",
+      error: "/sign-in",
+    },
+    callbacks: {
+      async jwt({ token, user }: { token: JWT, user: User }) {
+        if (user) {
+          token.id = user.id;
+          token.name = user.name;
+          token.lastName = user.lastName;
+          token.email = user.email;
+          token.image = user.image;
+          token.createdAt = user.createdAt;
+          token.updatedAt = user.updatedAt;
+        }
+        return token;
+      },
+      async session({ session, token }: { session: Session, token: JWT }) {
+        if (session.user) {
+          session.user.id = token.id as string;
+          session.user.name = token.name as string;
+          session.user.email = token.email as string;
+          session.user.image = token.image as string | null;
+          session.user.lastName = token.lastName as string;
+          session.user.createdAt = token.createdAt as Date;
+          session.user.updatedAt = token.updatedAt as Date;
+        }
+        return session;
+      }
+    }
   })
