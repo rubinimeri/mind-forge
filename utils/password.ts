d1f62c39c2167d951b83b94bf235cd9a1f663585
Rubin Imeri <rubinimeri6@gmail.com>
2025-07-29 15:11:36 +0200

    Add proper proper type to hashAndSaltPassword func

--- utils/password.ts
+++ utils/password.ts
@@ -1,12 +1,12 @@
-import crypto from "crypto";
+import * as crypto from "crypto";
 
-function generateSalt() {
+export function generateSalt(): string {
   return crypto.randomBytes(16).toString("hex").normalize();
 }
 
-export function hashAndSaltPassword(password: string) {
+export function hashAndSaltPassword(password: string, salt: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    crypto.scrypt(password.normalize(), generateSalt(), 64, (err, hash) => {
+    crypto.scrypt(password.normalize(), salt, 64, (err, hash) => {
       if (err) reject(err)
 
       resolve(hash.toString("hex").normalize())
