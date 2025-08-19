8c76b7113f24cb6b3284e5f34b5917febfd6aa87
Rubin Imeri <rubinimeri6@gmail.com>
2025-08-19 11:39:15 +0200

    Add thought-details.tsx component

---
+++ app/(main)/dashboard/thought-details.tsx
@@ -0,0 +1,106 @@
+"use client"
+
+import {AIResponse, Task, Thought} from "@/prisma/app/generated/prisma";
+import {Check, ChevronDown, ChevronUp, Dot, FileText, Lightbulb, List, Tags, Target} from "lucide-react";
+import {Badge} from "@/components/ui/badge";
+import SaveToKanbanButton from "@/components/save-to-kanban-button";
+import {CardContent} from "@/components/ui/card";
+import {useState} from "react";
+import {Button} from "@/components/ui/button";
+
+type ThoughtWithResponse = Omit<Thought, "createdAt"> & {
+  createdAt: string;
+  AIResponse: AIResponse & {
+    tasks: (Omit<Task, "createdAt"> & { createdAt: string })[];
+  }
+}
+
+function ThoughtDetails({ thought }: { thought: ThoughtWithResponse }) {
+  const [showMore, setShowMore] = useState(false)
+
+  if (!showMore)
+    return (
+      <CardContent>
+        <Button
+          variant={"outline"}
+          className={"border-none p-0"}
+          onClick={() => setShowMore(true)}
+        >
+          Show More <ChevronDown/>
+        </Button>
+      </CardContent>
+    )
+
+  return (
+    <CardContent className={"space-y-6"} >
+      <Button
+        variant={"outline"}
+        className={"border-none p-0"}
+        onClick={() => setShowMore(false)}
+      >
+        Show Less <ChevronUp />
+      </Button>
+      <div className={"space-y-2"}>
+        <h3>
+          <FileText strokeWidth={1} size={20} className={"text-primary"}/>
+          Summary
+        </h3>
+        <p className={"tracking-wider text-sm font-light"}>
+          {thought.AIResponse.summary}
+        </p>
+      </div>
+
+      <div className={"space-y-2"}>
+        <h3>
+          <Lightbulb strokeWidth={1} size={20} className={"text-primary"}/>
+          Insight
+        </h3>
+        <p className={"tracking-wider text-sm font-light"}>
+          {thought.AIResponse.insight}
+        </p>
+      </div>
+
+      <div className={"space-y-2"}>
+        <h3>
+          <Target strokeWidth={1} size={20} className={"text-primary"}/>
+          Suggested Goal
+        </h3>
+        <p className={"tracking-wider text-sm font-light flex gap-3 items-center"}>
+          {thought.AIResponse.suggestedGoal} <span className={"bg-gray-50 rounded-full p-1"}><Check size={20} className={"text-green-500"}/></span>
+        </p>
+      </div>
+
+      <div className={"space-y-2"}>
+        <h3>
+          <Tags strokeWidth={1} size={20} className={"text-primary"}/>
+          Themes
+        </h3>
+        <ul className={"flex flex-wrap gap-3"}>
+          {thought.AIResponse.themes.map((theme: string, index) =>
+            (<Badge key={index} >
+              {theme[0].toUpperCase() + theme.slice(1)}
+            </Badge>))}
+        </ul>
+      </div>
+
+      <div className={"space-y-2"}>
+        <h3>
+          <List strokeWidth={1} size={20} className={"text-primary"}/>
+          Suggested Tasks
+        </h3>
+        <ul className={"w-full flex flex-col gap-3 justify-start"}>
+          {thought.AIResponse.tasks.map((task, index) => (
+            <li className={"flex justify-between"} key={index}>
+              <p className={"flex gap-2 items-center"}><Dot/>
+                {task.content}
+              </p>
+              <SaveToKanbanButton taskId={task.id} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </CardContent>
+  );
+}
+
+export default ThoughtDetails;
