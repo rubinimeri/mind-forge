34d4a5f6d16ce8e1cbb6ecd151b6eced78b03bec
Rubin Imeri <rubinimeri6@gmail.com>
2025-08-21 12:39:53 +0200

    Add faker library to create placeholder data

--- lib/placeholder-data.ts
+++ lib/placeholder-data.ts
@@ -1,3 +1,6 @@
+import {faker} from "@faker-js/faker";
+import {capitalize} from "@/lib/utils";
+
 export const placeholderThoughtData = [
   {
     id: '7dd151be-cb20-4051-b725-ffa94baa13191',
@@ -246,3 +249,19 @@ export const placeHolderTasks = [
     columnId: null
   }
 ]
+
+export const columns = [
+  { id: faker.string.uuid(), name: 'Planned', color: '#6B7280' },
+  { id: faker.string.uuid(), name: 'In Progress', color: '#F59E0B' },
+  { id: faker.string.uuid(), name: 'Done', color: '#10B981' },
+];
+
+export const exampleTasks = Array.from({ length: 20 })
+  .fill(null)
+  .map(() => ({
+    id: faker.string.uuid(),
+    name: capitalize(faker.company.buzzPhrase()),
+    startAt: faker.date.past({ years: 0.5, refDate: new Date() }),
+    endAt: faker.date.future({ years: 0.5, refDate: new Date() }),
+    column: faker.helpers.arrayElement(columns).id,
+  }));
