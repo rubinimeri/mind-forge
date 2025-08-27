3547ac79532505f84a8d1ae2d3df69065725c702
Rubin Imeri <rubinimeri6@gmail.com>
2025-08-27 12:26:38 +0200

    Rename chart component & import to page.tsx

--- app/(main)/insights/page.tsx
+++ app/(main)/insights/page.tsx
@@ -1,12 +1,16 @@
 import {auth} from "@/auth";
 import {redirect} from "next/navigation";
+import ThoughtsChartArea from "@/app/(main)/insights/thoughts-chart-area";
 
 async function Page() {
   const session = await auth();
 
   if (!session.user) redirect('/sign-in')
   return (
-    <h1>Insights</h1>
+    <>
+      <h1>Insights</h1>
+      <ThoughtsChartArea />
+    </>
   );
 }
 
---
+++ app/(main)/insights/thoughts-chart-area.tsx
@@ -0,0 +1,107 @@
+"use client"
+
+import * as React from "react"
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+import {chartData} from "@/lib/placeholder-data";
+
+const chartConfig = {
+  thoughtsCaptured: {
+    label: "Thoughts",
+    color: "var(--chart-2)",
+  },
+} satisfies ChartConfig
+
+export default function ThoughtsChartArea() {
+
+  const reversedData = [...chartData].reverse();
+
+  return (
+    <Card className="pt-0">
+      <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
+        <div className="grid flex-1 gap-1">
+          <CardTitle>Thoughts Captured</CardTitle>
+          <CardDescription>
+            Showing total thoughts captured per day.
+          </CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+        <ChartContainer
+          config={chartConfig}
+          className="aspect-auto h-[250px] w-full"
+        >
+          <AreaChart data={reversedData}>
+            <defs>
+              <linearGradient id="fillChart" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--color-thoughtsCaptured)"
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor="var(--color-thoughtsCaptured)"
+                  stopOpacity={0.1}
+                />
+              </linearGradient>
+            </defs>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="date"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              minTickGap={32}
+              tickFormatter={(value) => {
+                const date = new Date(value)
+                return date.toLocaleDateString("en-US", {
+                  month: "short",
+                  day: "numeric",
+                })
+              }}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={
+                <ChartTooltipContent
+                  label={"Thoughts Captured"}
+                  labelFormatter={(value) => {
+                    return new Date(value).toLocaleDateString("en-US", {
+                      month: "short",
+                      day: "numeric",
+                    })
+                  }}
+                  indicator="dot"
+                />
+              }
+            />
+            <Area
+              dataKey="thoughtsCaptured"
+              type="natural"
+              fill="url(#fillChart)"
+              stroke="var(--color-thoughtsCaptured)"
+              stackId="a"
+            />
+            <ChartLegend content={<ChartLegendContent />} />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  )
+}
