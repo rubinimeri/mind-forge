022ba8132e20929279799c1088360c93024b7893
Rubin Imeri <rubinimeri6@gmail.com>
2025-10-29 22:43:16 +0100

    fix: Disable organize thought button while thought is being generated

--- app/(main)/ai-response.tsx
+++ app/(main)/ai-response.tsx
@@ -1,9 +1,6 @@
-"use client"
+"use client";
 
-import {
-  useState,
-  useActionState
-} from "react";
+import { useState, useActionState } from "react";
 import {
   Bot,
   Dot,
@@ -11,13 +8,13 @@ import {
   Lightbulb,
   List,
   Tags,
-  Target
+  Target,
 } from "lucide-react";
 
 import SaveToDashboardButton from "@/components/save-to-dashboard-button";
 import SaveToKanbanButton from "@/components/save-to-kanban-button";
 import TypingEffect from "@/components/typing-effect";
-import { createThought } from '@/app/actions'
+import { createThought } from "@/app/actions";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
@@ -26,139 +23,210 @@ import {
   Card,
   CardContent,
   CardFooter,
-  CardHeader
+  CardHeader,
 } from "@/components/ui/card";
 
-import {
-  AIResponse,
-  Task,
-  Thought
-} from "@/prisma/app/generated/prisma";
+import { AIResponse, Task, Thought } from "@/prisma/app/generated/prisma";
 
 type ThoughtState = {
-  thought: Thought,
-  AIResponse: AIResponse,
-  tasks: Task[]
-}
+  thought: Thought;
+  AIResponse: AIResponse;
+  tasks: Task[];
+};
 
 export default function AIResponseCard() {
   const [currentRenderIndex, setCurrentRenderIndex] = useState(0);
-  const [state, formAction, statePending] =
-    useActionState<Promise<ThoughtState | null>, FormData>(createThought, null)
-
+  const [state, formAction, statePending] = useActionState<
+    Promise<ThoughtState | null>,
+    FormData
+  >(createThought, null);
 
   return (
     <>
       <Card className={"mx-auto max-w-lg mt-8"}>
         <CardHeader>
           <h1
-            className={"mx-auto text-primary leading-tighter max-w-2xl text-4xl font-semibold tracking-tight text-balance lg:leading-[1.1] lg:font-semibold xl:text-5xl xl:tracking-tighter text-center"}>
-            What's on your mind?
+            className={
+              "mx-auto text-primary leading-tighter max-w-2xl text-4xl font-semibold tracking-tight text-balance lg:leading-[1.1] lg:font-semibold xl:text-5xl xl:tracking-tighter text-center"
+            }
+          >
+            What&apos;s on your mind?
           </h1>
         </CardHeader>
         <CardContent>
           <form action={formAction}>
-            <Textarea name={"thought"} cols={20} placeholder={"Type your thought here."} className={"placeholder:text-gray-400 border-2 border-inherit"}></Textarea>
-            <Button type="submit" className={"mt-4 w-full"} onClick={() => setCurrentRenderIndex(0)}>Organize thought</Button>
+            <Textarea
+              name={"thought"}
+              cols={20}
+              placeholder={"Type your thought here."}
+              className={"placeholder:text-gray-400 border-2 border-inherit"}
+            ></Textarea>
+            <Button
+              type="submit"
+              className={"mt-4 w-full"}
+              onClick={() => setCurrentRenderIndex(0)}
+              disabled={statePending}
+            >
+              Organize thought
+            </Button>
           </form>
         </CardContent>
       </Card>
-      { (!statePending && state) &&
-        <Card className={"mx-auto max-w-lg mt-8 animate-[slideUp_0.5s_ease-out_forwards] mb-8"}>
-        <CardHeader className={"flex justify-between items-center"}>
-          {currentRenderIndex >= 0 && (
-            <h2
-              className={"text-secondary leading-tighter text-2xl font-light tracking-tight text-balance lg:leading-[1.1] xl:text-3xl xl:tracking-tighter flex items-center gap-1"}>
-              <Bot strokeWidth={1} size={30} className={"text-primary"}/>
+      {!statePending && state && (
+        <Card
+          className={
+            "mx-auto max-w-lg mt-8 animate-[slideUp_0.5s_ease-out_forwards] mb-8"
+          }
+        >
+          <CardHeader className={"flex justify-between items-center"}>
+            {currentRenderIndex >= 0 && (
+              <h2
+                className={
+                  "text-secondary leading-tighter text-2xl font-light tracking-tight text-balance lg:leading-[1.1] xl:text-3xl xl:tracking-tighter flex items-center gap-1"
+                }
+              >
+                <Bot strokeWidth={1} size={30} className={"text-primary"} />
                 <TypingEffect
                   text="AI Reflection"
                   onDoneAction={() => setCurrentRenderIndex(1)}
                 />
-            </h2>
-          )}
-          {currentRenderIndex >= 1 && (
-            <SaveToDashboardButton
-              onDoneAction={setCurrentRenderIndex}
-              thoughtId={state.thought.id}
-            />
-          )}
-        </CardHeader>
-        <CardContent className={"space-y-4"}>
-          {currentRenderIndex >= 2 && (
-            <h3>
-              <FileText strokeWidth={1} size={20} className={"text-primary"}/>
-              <TypingEffect text={"Summary"} onDoneAction={() => setCurrentRenderIndex(3)} />
-            </h3>
-          )}
-          {currentRenderIndex >= 3 && (
-            <p className={"tracking-wider text-sm font-light"}>
-              <TypingEffect text={state.AIResponse.summary} onDoneAction={() => setCurrentRenderIndex(4)} />
-            </p>
-          )}
-          {currentRenderIndex >= 4 && (
-            <h3>
-              <Lightbulb strokeWidth={1} size={20} className={"text-primary"}/>
-              <TypingEffect text={"Insight"} onDoneAction={() => setCurrentRenderIndex(5)} />
-            </h3>
-          )}
-          {currentRenderIndex >= 5 && (
-            <p className={"tracking-wider text-sm font-light"}>
-              <TypingEffect text={state.AIResponse.insight} onDoneAction={() => setCurrentRenderIndex(6)} />
-            </p>
-          )}
-          {currentRenderIndex >= 6 && (
-           <h3>
-             <Target strokeWidth={1} size={20} className={"text-primary"}/>
-             <TypingEffect text={"Suggested goal"} onDoneAction={() => setCurrentRenderIndex(7)} />
-           </h3>
-          )}
-          {currentRenderIndex >= 7 && (
-            <p className={"tracking-wider text-sm font-light"}>
-              <TypingEffect text={state.AIResponse.suggestedGoal} onDoneAction={() => setCurrentRenderIndex(8)} />
-            </p>
-          )}
-          {currentRenderIndex >= 8 && (
-            <h3>
-              <Tags strokeWidth={1} size={20} className={"text-primary"}/>
-              <TypingEffect text={"Themes"} onDoneAction={() => setCurrentRenderIndex(9)} />
-            </h3>
-          )}
-          {currentRenderIndex >= 9 && (
-            <ul className={"flex flex-wrap gap-3"}>
-              {state.AIResponse.themes.map((theme: string, index) =>
-                (<Badge key={index} >
-                  <TypingEffect text={theme[0].toUpperCase() + theme.slice(1)} onDoneAction={() => setCurrentRenderIndex(10)} />
-                </Badge>))}
-            </ul>
-          )}
-        </CardContent>
-        <CardFooter className={"flex flex-col items-start space-y-4 font-light tracking-wide"}>
-          {currentRenderIndex >= 10 && (
-            <h3>
-              <List strokeWidth={1} size={20} className={"text-primary"}/>
-              <TypingEffect text={"Suggested Tasks"} onDoneAction={() => setCurrentRenderIndex(11)} />
-            </h3>
-          )}
-          {currentRenderIndex >= 11 && (
-          <ul className={"w-full flex flex-col gap-3 justify-start"}>
-            {state.tasks.map((task, index) => (
-              <li className={"flex justify-between"} key={index}>
-                <p className={"flex gap-2 items-center"}><Dot/>
-                  <TypingEffect text={task.content} onDoneAction={() => setCurrentRenderIndex(11)} />
-                </p>
-                <SaveToKanbanButton taskId={task.id} />
-              </li>
-            ))}
-          </ul>
-          )}
-        </CardFooter>
-      </Card>
-      }
-      { (statePending) &&
-        <Card className={"mx-auto max-w-lg mt-8 h-[50svh] py-0 animate-[slideUp_0.5s_ease-out_forwards]"} >
-          <Skeleton className={"h-full bg-gradient-to-r from-muted via-muted-foreground/20 to-muted bg-[length:200%_100%] animate-[shimmer_3s_infinite]"} />
+              </h2>
+            )}
+            {currentRenderIndex >= 1 && (
+              <SaveToDashboardButton
+                onDoneAction={setCurrentRenderIndex}
+                thoughtId={state.thought.id}
+              />
+            )}
+          </CardHeader>
+          <CardContent className={"space-y-4"}>
+            {currentRenderIndex >= 2 && (
+              <h3>
+                <FileText
+                  strokeWidth={1}
+                  size={20}
+                  className={"text-primary"}
+                />
+                <TypingEffect
+                  text={"Summary"}
+                  onDoneAction={() => setCurrentRenderIndex(3)}
+                />
+              </h3>
+            )}
+            {currentRenderIndex >= 3 && (
+              <p className={"tracking-wider text-sm font-light"}>
+                <TypingEffect
+                  text={state.AIResponse.summary}
+                  onDoneAction={() => setCurrentRenderIndex(4)}
+                />
+              </p>
+            )}
+            {currentRenderIndex >= 4 && (
+              <h3>
+                <Lightbulb
+                  strokeWidth={1}
+                  size={20}
+                  className={"text-primary"}
+                />
+                <TypingEffect
+                  text={"Insight"}
+                  onDoneAction={() => setCurrentRenderIndex(5)}
+                />
+              </h3>
+            )}
+            {currentRenderIndex >= 5 && (
+              <p className={"tracking-wider text-sm font-light"}>
+                <TypingEffect
+                  text={state.AIResponse.insight}
+                  onDoneAction={() => setCurrentRenderIndex(6)}
+                />
+              </p>
+            )}
+            {currentRenderIndex >= 6 && (
+              <h3>
+                <Target strokeWidth={1} size={20} className={"text-primary"} />
+                <TypingEffect
+                  text={"Suggested goal"}
+                  onDoneAction={() => setCurrentRenderIndex(7)}
+                />
+              </h3>
+            )}
+            {currentRenderIndex >= 7 && (
+              <p className={"tracking-wider text-sm font-light"}>
+                <TypingEffect
+                  text={state.AIResponse.suggestedGoal}
+                  onDoneAction={() => setCurrentRenderIndex(8)}
+                />
+              </p>
+            )}
+            {currentRenderIndex >= 8 && (
+              <h3>
+                <Tags strokeWidth={1} size={20} className={"text-primary"} />
+                <TypingEffect
+                  text={"Themes"}
+                  onDoneAction={() => setCurrentRenderIndex(9)}
+                />
+              </h3>
+            )}
+            {currentRenderIndex >= 9 && (
+              <ul className={"flex flex-wrap gap-3"}>
+                {state.AIResponse.themes.map((theme: string, index) => (
+                  <Badge key={index}>
+                    <TypingEffect
+                      text={theme[0].toUpperCase() + theme.slice(1)}
+                      onDoneAction={() => setCurrentRenderIndex(10)}
+                    />
+                  </Badge>
+                ))}
+              </ul>
+            )}
+          </CardContent>
+          <CardFooter
+            className={
+              "flex flex-col items-start space-y-4 font-light tracking-wide"
+            }
+          >
+            {currentRenderIndex >= 10 && (
+              <h3>
+                <List strokeWidth={1} size={20} className={"text-primary"} />
+                <TypingEffect
+                  text={"Suggested Tasks"}
+                  onDoneAction={() => setCurrentRenderIndex(11)}
+                />
+              </h3>
+            )}
+            {currentRenderIndex >= 11 && (
+              <ul className={"w-full flex flex-col gap-3 justify-start"}>
+                {state.tasks.map((task, index) => (
+                  <li className={"flex justify-between"} key={index}>
+                    <p className={"flex gap-2 items-center"}>
+                      <Dot />
+                      <TypingEffect
+                        text={task.content}
+                        onDoneAction={() => setCurrentRenderIndex(11)}
+                      />
+                    </p>
+                    <SaveToKanbanButton taskId={task.id} />
+                  </li>
+                ))}
+              </ul>
+            )}
+          </CardFooter>
         </Card>
-      }
+      )}
+      {statePending && (
+        <Card
+          className={
+            "mx-auto max-w-lg mt-8 h-[50svh] py-0 animate-[slideUp_0.5s_ease-out_forwards]"
+          }
+        >
+          <Skeleton
+            className={
+              "h-full bg-gradient-to-r from-muted via-muted-foreground/20 to-muted bg-[length:200%_100%] animate-[shimmer_3s_infinite]"
+            }
+          />
+        </Card>
+      )}
     </>
   );
 }
